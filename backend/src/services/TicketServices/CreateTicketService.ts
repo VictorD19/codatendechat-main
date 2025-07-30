3a0455c778526a5e7bc81575c78c4403a01f0ef1
Victor <bertramvictor61@gmail.com>
2025-07-30 01:01:33 -0300

    feat: adicinão de log s

--- backend/src/services/TicketServices/CreateTicketService.ts
+++ backend/src/services/TicketServices/CreateTicketService.ts
@@ -6,6 +6,7 @@ import ShowContactService from "../ContactServices/ShowContactService";
 import { getIO } from "../../libs/socket";
 import GetDefaultWhatsAppByUser from "../../helpers/GetDefaultWhatsAppByUser";
 import ShowWhatsAppService from "../WhatsappService/ShowWhatsAppService";
+import { logger } from "../../utils/logger";
 
 interface Request {
   contactId: number;
@@ -25,23 +26,24 @@ const CreateTicketService = async ({
   whatsappId
 }: Request): Promise<Ticket> => {
   let whatsapp;
-
-  if (whatsappId !== undefined && whatsappId !== null && whatsappId !==  "") {
+  logger.info("Começando criação message")
+  if (whatsappId !== undefined && whatsappId !== null && whatsappId !== "") {
     whatsapp = await ShowWhatsAppService(whatsappId, companyId)
   }
-  
-  let defaultWhatsapp = await GetDefaultWhatsAppByUser(userId);
+  logger.info(`Intancia encontrada: ${whatsapp != null}`)
 
+  let defaultWhatsapp = await GetDefaultWhatsAppByUser(userId);
+  logger.info(`Intancia DEFAULT: ${defaultWhatsapp != null}`)
   if (whatsapp) {
     defaultWhatsapp = whatsapp;
   }
   if (!defaultWhatsapp)
     defaultWhatsapp = await GetDefaultWhatsApp(companyId);
-
+  logger.info(`Chegarem de contato`)
   await CheckContactOpenTickets(contactId, whatsappId);
 
   const { isGroup } = await ShowContactService(contactId, companyId);
-
+  logger.info(`Inicio Criação`)
   const [{ id }] = await Ticket.findOrCreate({
     where: {
       contactId,
@@ -57,12 +59,12 @@ const CreateTicketService = async ({
       userId
     }
   });
-
+  logger.info(`Fim Criação`)
   await Ticket.update(
     { companyId, queueId, userId, whatsappId: defaultWhatsapp.id, status: "open" },
     { where: { id } }
   );
-
+  logger.info(`Atualiza Criação`)
   const ticket = await Ticket.findByPk(id, { include: ["contact", "queue"] });
 
   if (!ticket) {
